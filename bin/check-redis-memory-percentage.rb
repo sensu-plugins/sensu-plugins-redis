bd6424048652db03956a3559829d667f0f8d5deb
Hunter Satterwhite <hsatterwhite@envintus.com>
2016-04-06 16:34:54 -0400

    use double quotes so the variable is interpolated

--- bin/check-redis-memory-percentage.rb
+++ bin/check-redis-memory-percentage.rb
@@ -86,7 +86,7 @@ class RedisChecks < Sensu::Plugin::Check::CLI
     elsif used_memory >= warn_memory
       warning "Redis running on #{config[:host]}:#{config[:port]} is above the WARNING limit: #{used_memory} % used / #{warn_memory}%"
     else
-      ok 'Redis memory usage: #{used_memory} is below defined limits'
+      ok "Redis memory usage: #{used_memory} is below defined limits"
     end
   rescue
     message = "Could not connect to Redis server on #{config[:host]}:#{config[:port]}"
